fa6f801996bcf46243ba5d30cdb7a4724890ded3
Trung Dinh Quang <trungdq88@gmail.com>
2016-01-03 20:51:52 +0700

    Random select an item

--- remind.js
+++ remind.js
@@ -1,4 +1,14 @@
 require('shelljs/global');
+var fs = require('fs');
+
+fs.readFile('data.json', 'utf8', function (err, data) {
+  if (!err) {
+    data = JSON.parse(data);
+    onReady(data);
+  } else {
+    console.error(err);
+  }
+});
 
 var ask = function (question, defaultAnswer, callback) {
   var escape = function (str) {
@@ -17,13 +27,25 @@ var ask = function (question, defaultAnswer, callback) {
 
   var command = 'osascript -e \'Tell application "System Events" to display dialog "' + question + '" default answer "' + defaultAnswer + '"\' -e \'text returned of result\' 2>/dev/null';
   if (!callback) {
-    return exec(command, {silent: true}).output
+    return exec(command, {silent: true}).output.trim();
   } else {
     exec(command);
   }
 }
 
-// sync call to exec()
-var answer = ask("What is HoaPTT number?");
+var onReady = function (data) {
+  var keys = Object.keys(data);
+  console.log(data);
+  var key = keys[Math.floor(Math.random() * keys.length)];
+  var result = false;
+  while (!result) {
+    var answer = ask(key);
+    console.log(answer, ' - ', data[key]);
+    if (answer === data[key]) {
+      result = true;
+    } else {
+      result = false;
+    }
+  }
+};
 
-console.log(answer);
